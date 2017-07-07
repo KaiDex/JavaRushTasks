75c8485d2675a0e5920f8b01dfefaeadf7a39dfd
KaiDex <2039666@gmail.com>
2017-07-07 09:58:49 +0300

    Add files via upload

---
+++ MyWebDB/target/MyWebDB/WEB-INF/classes/database/fillDB.sql
@@ -0,0 +1,10 @@
+INSERT INTO test.users (ID, NAME, AGE, IS_ADMIN, CREATED_DATE) VALUES ('1', 'Vladimir Putin', 64, true, NULL );
+INSERT INTO test.users (ID, NAME, AGE, IS_ADMIN, CREATED_DATE) VALUES ('2', '123321', 10, true, NULL );
+INSERT INTO test.users (ID, NAME, AGE, IS_ADMIN, CREATED_DATE) VALUES ('3', 'Gender Bender', 4, true, NULL );
+INSERT INTO test.users (ID, NAME, AGE, IS_ADMIN, CREATED_DATE) VALUES ('4', 'Admin', 50, false, NULL );
+INSERT INTO test.users (ID, NAME, AGE, IS_ADMIN, CREATED_DATE) VALUES ('5', 'qwerty', 1, false, NULL );
+INSERT INTO test.users (ID, NAME, AGE, IS_ADMIN, CREATED_DATE) VALUES ('6', 'ytrewq', 33, true, NULL );
+INSERT INTO test.users (ID, NAME, AGE, IS_ADMIN, CREATED_DATE) VALUES ('7', 'Kurt Donald Cobain', 27, false, NULL );
+INSERT INTO test.users (ID, NAME, AGE, IS_ADMIN, CREATED_DATE) VALUES ('8', 'Invalid_Name', 200, false, NULL );
+INSERT INTO test.users (ID, NAME, AGE, IS_ADMIN, CREATED_DATE) VALUES ('9', 'Ave, Rege', 666, true, NULL );
+INSERT INTO test.users (ID, NAME, AGE, IS_ADMIN, CREATED_DATE) VALUES ('10', 'Lucky_777_Number', 13, false, NULL );
